80c97c4b1b04a86afa845d8dbe36fa1326504958
emiel steerneman <emielsteerneman@gmail.com>
2022-12-01 21:04:55 +0100

    I like this more

--- day1/day1.py
+++ day1/day1.py
@@ -1,7 +1,7 @@
 text = open("input.txt").read()
 
 elves_inventory = text.split("\n\n")
-elves_inventory = [ [ int(_) for _ in inv.split("\n")] for inv in elves_inventory ]
+elves_inventory = [ [ *map(int, inv.split("\n")) ] for inv in elves_inventory ]
 
 elves_inventory_summation = [ sum(_) for _ in elves_inventory ]
 
