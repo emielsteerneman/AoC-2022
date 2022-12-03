379a162f29eb2e09415cfb31e781d52cfcf7748d
emiel steerneman <emielsteerneman@gmail.com>
2022-12-03 12:32:55 +0100

    removed useless []

--- day3/day3.py
+++ day3/day3.py
@@ -4,8 +4,8 @@ containers = [ [_[:len(_)//2], _[len(_)//2:]] for _ in bags ]
 s = lambda s : list(set.intersection(*map(set, s)))[0]
 m = lambda c : ord(c) - 38 - 58 * c.islower()
 
-part1 = sum( [ m(s(_)) for _ in containers ] )
-part2 = sum( [ m(s(_)) for _ in zip(*[iter(bags)]*3) ])
+part1 = sum( m(s(_)) for _ in containers )
+part2 = sum( m(s(_)) for _ in zip(*[iter(bags)]*3) )
 
 print("Part 1:", part1)
 print("Part 2:", part2)
